acc8b60b4cbde2499c97f0591bae027bf322e093
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-01-08 21:14:48 +0600

    Module F2.3 done

--- 00. Mission 6/Module_F2/testProject2/src/pages/UseStateExample.tsx
+++ 00. Mission 6/Module_F2/testProject2/src/pages/UseStateExample.tsx
@@ -2,22 +2,25 @@ import { useState } from "react";
 
 const UseStateExample = () => {
   const [counter, setCounter] = useState(0);
-  console.log("render");
+
+  const handleIncrement = () => {
+    setCounter((prevState) => prevState + 1);
+  };
+
+  const handleAsyncIncrement = () => {
+    setTimeout(() => {
+      setCounter((prevState) => prevState + 1);
+    }, 2000);
+  };
 
   return (
     <div className="flex flex-col space-y-3 items-center h-screen justify-center">
       <h1>Count: {counter}</h1>
-      <button
-        className="bg-green-300 px-4 py-2 rounded"
-        onClick={() => setCounter((prevState) => prevState + 1)}
-      >
+      <button className="bg-green-300 px-4 py-2 rounded" onClick={handleIncrement}>
         Increment
       </button>
-      <button
-        className="bg-red-300 px-4 py-2 rounded"
-        onClick={() => setCounter((prevState) => prevState - 1)}
-      >
-        Increment
+      <button className="bg-red-300 px-4 py-2 rounded" onClick={handleAsyncIncrement}>
+        handleAsyncIncrement
       </button>
       <button className="bg-gray-300 w-24 px-4 py-2 rounded" onClick={() => setCounter(0)}>
         Reset
