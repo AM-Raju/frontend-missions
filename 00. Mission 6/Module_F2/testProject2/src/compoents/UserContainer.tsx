74bf7269dcd9795df663d0952be53ffbbe4a45c1
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-01-13 11:38:49 +0600

    Module F3.8 done

--- 00. Mission 6/Module_F2/testProject2/src/compoents/UserContainer.tsx
+++ 00. Mission 6/Module_F2/testProject2/src/compoents/UserContainer.tsx
@@ -1,29 +1,8 @@
-import { useEffect, useState } from "react";
 import UserList from "./UserList";
+import useUserData from "../hooks/useUserData";
 
 const UserContainer = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [data, setData] = useState([]);
-
-  const url = "https://jsonplaceholder.typicode.com/users";
-
-  const getUsers = async () => {
-    setIsLoading(true);
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      setIsLoading(false);
-      setData(data);
-      console.log(data);
-    } catch (error) {
-      setError(true);
-    }
-  };
-
-  useEffect(() => {
-    getUsers();
-  }, []);
+  const { isLoading, error, data } = useUserData();
 
   return <UserList error={error} data={data} isLoading={isLoading}></UserList>;
 };
--- 00. Mission 6/Module_F2/testProject2/src/hooks/useUserData.tsx
+++ 00. Mission 6/Module_F2/testProject2/src/hooks/useUserData.tsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
 const useUserData = () => {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState([]);
+
+  const controller = new AbortController();
+
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  const getUsers = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(url, { signal: controller.signal });
+      const data = await res.json();
+      setIsLoading(false);
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+  return { isLoading, error, data };
 };
 
 export default useUserData;
