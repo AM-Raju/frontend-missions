2744871c493ed62b3c59bc9c27608258105e37d6
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-01-15 09:28:27 +0600

    Module F4.7

--- 00. Mission 6/Module_F4/react-reusuable-components/src/components/ui/Button.tsx
+++ 00. Mission 6/Module_F4/react-reusuable-components/src/components/ui/Button.tsx
@@ -1,26 +1,41 @@
-import React from "react";
-import { twMerge } from "tailwind-merge";
-import { clsx } from "clsx";
+import React, { ButtonHTMLAttributes, DetailedHTMLProps, forwardRef } from "react";
 import cn from "../../utils/cn";
 
 /* `px-4 py-2 bg-red-500 ${className} ${
     variant === "outline" ? "border-2 border-purple-500 bg-opacity-10" : ""
   }` */
 
-const Button = ({ className, children, variant }) => {
-  const getVariant = (variant) => {
-    switch (variant) {
-      case "outline":
-        return "btn-outline";
-      case "ghost":
-        return "btn-outline";
+type TRef = HTMLButtonElement;
 
-      default:
-        return "btn-solid";
-    }
-  };
+type TVariant = "solid" | "ghost" | "outline";
 
-  return <button className={cn(getVariant(variant), className)}>{children}</button>;
+type TButtonOptions = {
+  variant?: TVariant;
 };
+
+type TButton = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> &
+  TButtonOptions;
+
+const Button = forwardRef<TRef, TButton>(
+  ({ className, children, variant = "solid", ...rest }, ref) => {
+    const getVariant = (variant: TVariant) => {
+      switch (variant) {
+        case "outline":
+          return "btn-outline";
+        case "ghost":
+          return "btn-outline";
+
+        default:
+          return "btn-solid";
+      }
+    };
+
+    return (
+      <button {...rest} ref={ref} className={cn(getVariant(variant), className)}>
+        {children}
+      </button>
+    );
+  }
+);
 
 export default Button;
