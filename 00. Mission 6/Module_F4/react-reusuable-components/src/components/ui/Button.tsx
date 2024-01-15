b8617321312854e41b5e5eb6a66966f6131b5ae2
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-01-15 06:50:16 +0600

    Module F4.5 done

--- 00. Mission 6/Module_F4/react-reusuable-components/src/components/ui/Button.tsx
+++ 00. Mission 6/Module_F4/react-reusuable-components/src/components/ui/Button.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
+import { clsx } from "clsx";
+import cn from "../../utils/cn";
 
-const Button = ({ className }: { className: string }) => {
-  return <button className={`bg-green-500 ${className}`}>Click</button>;
+/* `px-4 py-2 bg-red-500 ${className} ${
+    variant === "outline" ? "border-2 border-purple-500 bg-opacity-10" : ""
+  }` */
+
+const Button = ({ className, children, outline }: { className: string }) => {
+  return (
+    <button
+      className={cn(
+        "px-4 py-2 bg-red-500",
+        {
+          "border-2 border-purple-500 bg-opacity-10": outline,
+        },
+        className
+      )}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- 00. Mission 6/Module_F4/react-reusuable-components/src/pages/About.tsx
+++ 00. Mission 6/Module_F4/react-reusuable-components/src/pages/About.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Button from "../components/ui/Button";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="flex justify-center h-screen items-center">
+      <Button className="bg-green-500" outline={true}>
+        About Me Button
+      </Button>
+    </div>
+  );
 };
 
 export default About;
