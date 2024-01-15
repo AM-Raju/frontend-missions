2f40c1d0a76e9f0a6d866ae51da779f7f4df308f
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-01-15 07:27:13 +0600

    Module F4.6 done

--- 00. Mission 6/Module_F4/react-reusuable-components/src/components/ui/Button.tsx
+++ 00. Mission 6/Module_F4/react-reusuable-components/src/components/ui/Button.tsx
@@ -7,20 +7,20 @@ import cn from "../../utils/cn";
     variant === "outline" ? "border-2 border-purple-500 bg-opacity-10" : ""
   }` */
 
-const Button = ({ className, children, outline }: { className: string }) => {
-  return (
-    <button
-      className={cn(
-        "px-4 py-2 bg-red-500",
-        {
-          "border-2 border-purple-500 bg-opacity-10": outline,
-        },
-        className
-      )}
-    >
-      {children}
-    </button>
-  );
+const Button = ({ className, children, variant }) => {
+  const getVariant = (variant) => {
+    switch (variant) {
+      case "outline":
+        return "btn-outline";
+      case "ghost":
+        return "btn-outline";
+
+      default:
+        return "btn-solid";
+    }
+  };
+
+  return <button className={cn(getVariant(variant), className)}>{children}</button>;
 };
 
 export default Button;
--- 00. Mission 6/Module_F4/react-reusuable-components/src/pages/About.tsx
+++ 00. Mission 6/Module_F4/react-reusuable-components/src/pages/About.tsx
@@ -4,9 +4,11 @@ import Button from "../components/ui/Button";
 const About = () => {
   return (
     <div className="flex justify-center h-screen items-center">
-      <Button className="bg-green-500" outline={true}>
-        About Me Button
-      </Button>
+      <div className="w-96 p-10 border border-gray-300">
+        <Button className="w-full" variant="outline">
+          About Me Button
+        </Button>
+      </div>
     </div>
   );
 };
