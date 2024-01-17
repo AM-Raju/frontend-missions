6275d6276531da5e07c0c5d8950aa48ffea44ef4
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-01-17 14:35:34 +0600

    Module F5.3 done

--- 00. Mission 6/Module_F4/react-reusuable-components/src/components/NormalForm.tsx
+++ 00. Mission 6/Module_F4/react-reusuable-components/src/components/NormalForm.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import cn from "../utils/cn";
 
 const NormalForm = () => {
   const { register, handleSubmit } = useForm();
@@ -8,18 +9,35 @@ const NormalForm = () => {
     console.log(data);
   };
 
+  const double = true;
+
   return (
-    <form className="border border-red-500 max-w-4xl p-5 mx-auto" onSubmit={handleSubmit(onSubmit)}>
-      <div className="border border-purple-500 grid grid-cols-2 gap-3">
-        <div>
+    <form
+      className={cn("border border-red-500 max-w-md p-5 mx-auto", {
+        "max-w-5xl": double,
+        "max-w-md": !double,
+      })}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div
+        className={cn("border border-purple-500 grid justify-items-center  gap-3", {
+          "md:grid-cols-2": double,
+        })}
+      >
+        <div className="w-full max-w-md">
           <label htmlFor="name">Name</label>
-          <input className="block w-full" type="text" id="name" {...register("name")} />
+          <input
+            className="block w-full border border-gray-300 rounded-md focus:border-purple-500 focus:ring-1 focus:ring-purple-500"
+            type="text"
+            id="name"
+            {...register("name")}
+          />
         </div>
-        <div>
+        <div className="w-full max-w-md">
           <label htmlFor="name">Email</label>
           <input className="block w-full" type="email" id="email" {...register("email")} />
         </div>
-        <div>
+        <div className="w-full max-w-md">
           <label htmlFor="name">Password</label>
           <input className="block w-full" type="password" id="password" {...register("password")} />
         </div>
