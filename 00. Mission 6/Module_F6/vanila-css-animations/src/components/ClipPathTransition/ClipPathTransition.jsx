599a3771064b82bb5abcb8dd4034c2fff59470c1
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-01-20 13:44:11 +0600

    Module F6.7 done

---
+++ 00. Mission 6/Module_F6/vanila-css-animations/src/components/ClipPathTransition/ClipPathTransition.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./ClipPathTransition.css";
+
+const ClipPathTransition = () => {
+  return (
+    <div className="main7">
+      <div className="card">
+        <h1>Heading</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente quis voluptate
+          voluptates eius pariatur dicta quisquam aperiam aliquam reiciendis voluptatum iste,
+          maiores commodi aspernatur quos, animi exercitationem aliquid harum fugiat explicabo
+          ullam! Ipsum minus debitis ratione cum molestias unde assumenda quisquam tempore enim,
+          saepe accusantium aperiam, dolorem similique. Laboriosam, voluptates!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ClipPathTransition;
--- 00. Mission 6/Module_F6/vanila-css-animations/src/main.jsx
+++ 00. Mission 6/Module_F6/vanila-css-animations/src/main.jsx
@@ -5,12 +5,14 @@ import "./index.css";
 import Button from "./components/Button/Button.jsx";
 import AnimationBasic from "./components/animation-basic/AnimationBasic.jsx";
 import LoadingAnimation from "./components/LoadingAnimation/LoadingAnimation.jsx";
+import ClipPathTransition from "./components/ClipPathTransition/ClipPathTransition.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <Button></Button> */}
     {/* <AnimationBasic></AnimationBasic> */}
-    <LoadingAnimation></LoadingAnimation>
+    {/* <LoadingAnimation></LoadingAnimation> */}
+    <ClipPathTransition></ClipPathTransition>
   </React.StrictMode>
 );
