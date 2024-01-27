919423cd8c57d42daf3c82855ef3edc4b9484230
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-01-27 12:04:02 +0600

    Module F8.6 done

--- 00. Mission 6/Module_F8/src/pages/home/HeroSection.tsx
+++ 00. Mission 6/Module_F8/src/pages/home/HeroSection.tsx
@@ -4,21 +4,23 @@ import macbook from "@/assets/images/macbook-exposed.png";
 
 const HeroSection = () => {
   return (
-    <Container className="grid grid-cols-2 h-[calc(100vh-64px)]  place-content-center">
+    <Container className="grid grid-cols-1 lg:grid-cols-2 h-[calc(100vh-64px)]  place-content-center">
       <div>
-        <h1 className="text-8xl font-extrabold">
+        <h1 className="text-5xl lg:text-8xl font-extrabold text-nowrap">
           <span className="text-gray">Don't worry,</span>
           <br />
           <span>We'll fix it.</span>
         </h1>
-        <p>
-          Welcome to <span> iRepair </span>, your one-stop place for all kinds of{" "}
-          <span> Macbook repairs </span> and diagnostics.
+        <p className="max-w-[50ch] mt-10 mb-5 text-dark-gray">
+          Welcome to <span className="text-primary-foreground font-semibold"> iRepair </span>, your
+          one-stop place for all kinds of{" "}
+          <span className="text-primary-foreground font-semibold"> Macbook repairs </span> and
+          diagnostics.
         </p>
         <Button>Book and Service</Button>
       </div>
-      <div>
-        <img src={macbook} alt="" />
+      <div className="w-3/4 lg:w-full mx-auto">
+        <img className="-rotate-[35deg] h-[95%] object-contain" src={macbook} alt="" />
       </div>
     </Container>
   );
