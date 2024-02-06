3244a064eaf431fcde057c4dbee6bc9416d118bb
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-02-06 10:18:27 +0600

    Module F11.4 done

--- 00. Mission 6/nModule_F11/redux-start/src/App.tsx
+++ 00. Mission 6/nModule_F11/redux-start/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex gap-6 items-center border px-32 py-24 bg-gray-100">
+        <button className="bg-green-500 px-4 py-2 rounded-lg font-semibold ">Increment</button>
+        <h1 className="text-3xl">0</h1>
+        <button className="bg-red-500 px-4 py-2 rounded-lg font-semibold ">Decrement</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
