6c9db73a10b5b3cb126385486837e07de50970a4
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-02-06 12:40:52 +0600

    Module F11.5 done

--- 00. Mission 6/nModule_F11/redux-start/src/App.tsx
+++ 00. Mission 6/nModule_F11/redux-start/src/App.tsx
@@ -1,15 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { decrement, increment } from "./redux/features/counterSlice";
 
 function App() {
+  const { count } = useSelector((state) => state.counter);
+
+  const dispatch = useDispatch();
+
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex gap-6 items-center border px-32 py-24 bg-gray-100">
-        <button className="bg-green-500 px-4 py-2 rounded-lg font-semibold ">Increment</button>
-        <h1 className="text-3xl">0</h1>
-        <button className="bg-red-500 px-4 py-2 rounded-lg font-semibold ">Decrement</button>
+        <button
+          onClick={() => dispatch(increment())}
+          className="bg-green-500 px-4 py-2 rounded-lg font-semibold "
+        >
+          Increment
+        </button>
+        <h1 className="text-3xl">{count}</h1>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="bg-red-500 px-4 py-2 rounded-lg font-semibold "
+        >
+          Decrement
+        </button>
       </div>
     </div>
   );
--- 00. Mission 6/nModule_F11/redux-start/src/redux/features/counterSlice.tsx
+++ 00. Mission 6/nModule_F11/redux-start/src/redux/features/counterSlice.tsx
@@ -1,15 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface CounterState {
-  value: number;
+  count: number;
 }
 
 const initialState: CounterState = {
-  value: 0,
+  count: 0,
 };
 
 const counterSlice = createSlice({
   name: "counter",
   initialState,
-  reducers: {},
+  reducers: {
+    increment: (state, action) => {
+      state.count = state.count + 1;
+    },
+    decrement: (state, action) => {
+      state.count = state.count - 1;
+    },
+  },
 });
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement } = counterSlice.actions;
+
+export default counterSlice.reducer;
