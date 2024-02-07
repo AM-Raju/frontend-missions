551bbdd15fb4905a0ed04d891218b76629bc8887
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-02-07 17:57:32 +0600

    Module F12.4 done

--- 00. Mission 6/nModule_F12/redux-todo/src/components/todo/TodoCard.tsx
+++ 00. Mission 6/nModule_F12/redux-todo/src/components/todo/TodoCard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Button } from "../ui/button";
+import { Trash2, SquarePen } from "lucide-react";
 
 const TodoCard = () => {
   return (
@@ -7,9 +9,13 @@ const TodoCard = () => {
       <p className="font-semibold">Todo Title</p>
       <p>Time</p>
       <p>Description</p>
-      <div className="space-x-4">
-        <button>Delete</button>
-        <button>Edit</button>
+      <div className="space-x-7">
+        <Button className="bg-red-500">
+          <Trash2 />
+        </Button>
+        <Button className="bg-[#5c53fe]">
+          <SquarePen />
+        </Button>
       </div>
     </div>
   );
--- 00. Mission 6/nModule_F12/redux-todo/src/components/todo/TodoContainer.tsx
+++ 00. Mission 6/nModule_F12/redux-todo/src/components/todo/TodoContainer.tsx
@@ -1,17 +1,26 @@
+import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div>
-        <button>Add Todo</button>
-        <button>Filter</button>
+      <div className="flex justify-between items-center mb-5">
+        <AddTodoModal></AddTodoModal>
+        <Button className="bg-primary-gradient">Filter</Button>
       </div>
-      <div className="w-full bg-red-500 rounded-xl p-5 space-y-3">
+      <div className="w-full bg-primary-gradient rounded-xl p-1 ">
         {/* <div className="bg-white p-5 rounded-md flex justify-center items-center font-bold text-2xl">
           <p>There is no task pending</p>
         </div> */}
-        <TodoCard></TodoCard>
+        <div className="bg-white space-y-3 rounded-xl p-3 bg-opacity-20">
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+        </div>
       </div>
     </div>
   );
