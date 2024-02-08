e8d25ad49a7367c5c4484eb5c47225c2523d0dc7
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-02-08 19:13:54 +0600

    Module F12.10 done

--- 00. Mission 6/nModule_F12/redux-todo/src/components/todo/TodoContainer.tsx
+++ 00. Mission 6/nModule_F12/redux-todo/src/components/todo/TodoContainer.tsx
@@ -1,10 +1,19 @@
-import { useAppSelector } from "@/redux/hook";
+// import { useAppSelector } from "@/redux/hook";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
+import { useGetTodosQuery } from "@/redux/api/api";
 
 const TodoContainer = () => {
-  const { todos } = useAppSelector((state) => state.todos);
+  // Get data from local
+  // const { todos } = useAppSelector((state) => state.todos);
+
+  // Get data from server
+  const { data: todos, error, isLoading } = useGetTodosQuery(undefined);
+
+  if (isLoading) {
+    return <p>...Loading</p>;
+  }
 
   return (
     <div>
@@ -13,15 +22,15 @@ const TodoContainer = () => {
         <TodoFilter></TodoFilter>
       </div>
       <div className="w-full bg-primary-gradient rounded-xl p-1 ">
-        {todos.length < 1 && (
+        {todos?.data.length < 1 && (
           <div className="bg-white p-5 rounded-md flex justify-center items-center font-bold text-2xl">
             <p>There is no task pending</p>
           </div>
         )}
 
-        {todos.length > 0 && (
+        {todos?.data.length > 0 && (
           <div className="bg-white space-y-3 rounded-xl p-3 bg-opacity-20">
-            {todos.map((todo) => (
+            {todos?.data?.map((todo) => (
               <TodoCard key={todo?.id} todo={todo}></TodoCard>
             ))}
           </div>
