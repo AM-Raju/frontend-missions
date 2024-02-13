447e2a48cbffbe06ecc68f202de1e5cfc5624b6e
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-02-13 16:23:29 +0600

    ph-um-client Module F15.5 done

--- 00. Mission 6/nModule_F14/ph-um-client/src/pages/login/Login.tsx
+++ 00. Mission 6/nModule_F14/ph-um-client/src/pages/login/Login.tsx
@@ -1,7 +1,40 @@
+import { Button } from "antd";
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useLoginMutation } from "../../redux/features/auth/authApi";
+
+type TInputs = {
+  userId: string;
+  password: string;
+};
 
 const Login = () => {
-  return <div>This is login page.</div>;
+  const [login, { data, error }] = useLoginMutation();
+
+  const { register, handleSubmit } = useForm<TInputs>();
+
+  const onSubmit: SubmitHandler<TInputs> = (data) => {
+    console.log(data);
+    const loginInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+    login(loginInfo);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="userId">ID: </label>
+        <input defaultValue="A-001" {...register("userId")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input defaultValue="admin123" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Submit</Button>
+    </form>
+  );
 };
 
 export default Login;
