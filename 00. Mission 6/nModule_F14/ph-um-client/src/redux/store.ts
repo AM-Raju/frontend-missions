9341e322991fc618b0d45317716fb758090f2c0e
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-02-14 10:16:20 +0600

    ph-um-client Module F15.8 done

--- 00. Mission 6/nModule_F14/ph-um-client/src/redux/store.ts
+++ 00. Mission 6/nModule_F14/ph-um-client/src/redux/store.ts
@@ -1,14 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import authReducer from "./features/auth/authSlice";
 import { baseApi } from "./api/baseApi";
 
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 export const store = configureStore({
   reducer: {
     // for server state
     [baseApi.reducerPath]: baseApi.reducer,
     // for local state
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 });
@@ -17,3 +25,5 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
