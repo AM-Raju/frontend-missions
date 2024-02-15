572df9b3797ec9698dba3d5d3728763a0f342379
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-02-15 09:58:10 +0600

    ph-um-client Module F16.6 done

--- 00. Mission 6/nModule_F14/ph-um-client/src/redux/api/baseApi.ts
+++ 00. Mission 6/nModule_F14/ph-um-client/src/redux/api/baseApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
-import { setUser } from "../features/auth/authSlice";
+import { logout, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -17,7 +17,7 @@ const baseQuery = fetchBaseQuery({
 });
 
 const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
+  let result = await baseQuery(args, api, extraOptions);
   console.log(result);
   if (result.error?.status === 401) {
     const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
@@ -29,14 +29,18 @@ const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
 
     const user = (api.getState() as RootState).auth.user;
 
-    api.dispatch(
-      setUser({
-        user: user,
-        token: data.data.accessToken,
-      })
-    );
+    if (data?.data?.accessToken) {
+      api.dispatch(
+        setUser({
+          user: user,
+          token: data?.data?.accessToken,
+        })
+      );
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
   }
-
   return result;
 };
 
